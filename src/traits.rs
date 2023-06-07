13508fcde7fe9d06bea0e736a7cc41ab9594c958
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-06-06 22:00:07 -0400

    fix challenge 9
    I misunderstood it the first time

--- src/traits.rs
+++ src/traits.rs
@@ -1,11 +1,17 @@
+const PKCS7_PADDING_BYTES: [u8; 16] = [
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
+];
+
 pub trait Pad {
-    fn pad(self: &mut Self, number_of_bytes: usize) -> &mut Self;
+    fn pad_pkcs7(self: &mut Self) -> &mut Self;
 }
 
 impl Pad for Vec<u8> {
-    fn pad(self: &mut Self, number_of_bytes: usize) -> &mut Self {
-        for _n in 0..number_of_bytes {
-            self.push(0x04);
+    fn pad_pkcs7(self: &mut Self) -> &mut Self {
+        let num_of_bytes_needed = 16 - (self.len() % 16);
+
+        for _i in 0..num_of_bytes_needed {
+            self.push(PKCS7_PADDING_BYTES[num_of_bytes_needed - 1]);
         }
 
         self
--- tests/challenge_9.rs
+++ tests/challenge_9.rs
@@ -5,6 +5,21 @@ use cryptopals::traits::Pad;
 fn pad_bytes_with_pkcs7() {
     let mut bytes = ascii::string_to_bytes("YELLOW SUBMARINE");
 
-    assert_eq!(20, bytes.pad(4).len());
-    assert!(bytes.ends_with(&[0x04, 0x04, 0x04, 0x04]));
+    assert_eq!(32, bytes.pad_pkcs7().len());
+    assert!(bytes.ends_with(&[
+        0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+        0x10
+    ]));
+
+    let mut bytes = ascii::string_to_bytes("this is a test");
+
+    assert_eq!(16, bytes.pad_pkcs7().len());
+    assert!(bytes.ends_with(&[0x02, 0x02]));
+
+    let mut bytes = ascii::string_to_bytes("1");
+
+    assert_eq!(16, bytes.pad_pkcs7().len());
+    assert!(bytes.ends_with(&[
+        0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F
+    ]));
 }

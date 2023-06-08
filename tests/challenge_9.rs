d2ccc4e3e2f2a9d1a19e48ba57ee6f6edc3303f2
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-06-08 13:09:40 -0400

    split up challenge 9 tests

--- tests/challenge_9.rs
+++ tests/challenge_9.rs
@@ -10,12 +10,18 @@ fn pad_bytes_with_pkcs7() {
         0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
         0x10
     ]));
+}
 
+#[test]
+fn pad_14_bytes_with_pkcs7() {
     let mut bytes = ascii::string_to_bytes("this is a test");
 
     assert_eq!(16, bytes.pad_pkcs7().len());
     assert!(bytes.ends_with(&[0x02, 0x02]));
+}
 
+#[test]
+fn pad_1_byte_with_pkcs7() {
     let mut bytes = ascii::string_to_bytes("1");
 
     assert_eq!(16, bytes.pad_pkcs7().len());
